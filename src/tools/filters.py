f82a4f32dcfbbf3fceccbe0ac2fb8d6d225bfc46
Alex O'Neill <oneill.alexander.b@gmail.com>
2013-11-11 15:38:37 -0500

    Added a file to handle large changes to a Plane.
    Filters include a method to average raw values in the Plane to get
    smoother terrain, a method to flip the plane across it's horizontal
    plane and a method to "sample" th data in order to make the plane
    smaller.

---
+++ src/tools/filters.py
@@ -0,0 +1,45 @@
+import copy
+from src.tools import plane, neighborhood
+
+def flipSurface(dataPlane):
+  rawData = copy.deepcopy(dataPlane.rawData)
+
+  for row in xrange(dataPlane.height):
+    for col in xrange(dataPlane.width):
+      rawData[row][col] = -rawData[row][col] + dataPlane.maxVal
+
+  return plane.PlaneData(rawData)
+
+def sample(dataPlane, sampleStep):
+  rawData = dataPlane.rawData
+  newData = []
+  for row in xrange(0, dataPlane.height, sampleStep):
+    temp = []
+    for col in xrange(0, dataPlane.width, sampleStep):
+      temp += [rawData[row][col]]
+
+    newData += [temp]
+
+  return plane.PlaneData(newData)
+
+def averagePass(dataPlane, passNum = 1):
+  backupData = copy.deepcopy(dataPlane.rawData)
+
+  for loop in xrange(passNum):
+    reference = copy.deepcopy(backupData)
+
+    for row in xrange(dataPlane.height):
+      for col in xrange(dataPlane.width):
+
+        neighbors = neighborhood.getListNeighbors(row, col, reference)
+        sumOfNeighbors = 0
+        validNeighbors = 0
+
+        for neighbor in neighbors:
+          if(neighbor is not None):
+            sumOfNeighbors += neighbor
+            validNeighbors += 1
+
+        backupData[row][col] = (float(sumOfNeighbors) / validNeighbors)
+
+  return plane.PlaneData(backupData)
