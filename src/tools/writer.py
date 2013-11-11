78a6b8ccf7411d8b0849b47be105d211ebc6ece2
Alex O'Neill <oneill.alexander.b@gmail.com>
2013-11-11 15:37:40 -0500

    Added a file to handle writing a Plane to a CSV.

---
+++ src/tools/writer.py
@@ -0,0 +1,9 @@
+def writePlaneToFile(dataPlane, fileName):
+  f = open(fileName, "w")
+  for row in xrange(dataPlane.height):
+    temp = str(dataPlane.rawData[row][0])
+    for col in xrange(1, dataPlane.width):
+      temp += "," + str(dataPlane.rawData[row][col])
+
+    f.write(temp + "\n")
+  f.close()
